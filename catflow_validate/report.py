88540921328dcfbe6d62c5d6b3e4421987b3baed
Mirko Mälicke <mirko@hydrocode.de>
2023-01-15 14:09:35 +0100

    adding PAR validation to report

--- catflow_validate/report.py
+++ catflow_validate/report.py
@@ -2,6 +2,7 @@ import sys
 import contextlib
 
 import click
+from click import style, echo
 
 from catflow_validate.landuse import LanduseClassDef
 
@@ -20,9 +21,9 @@ class Report:
 
     def __run_with_echo(self):
         # header
-        click.echo("|--------------------------------------|")
-        click.echo("| CATFLOW input file validation report |")
-        click.echo("|--------------------------------------|")
+        echo("|--------------------------------------|")
+        echo("| CATFLOW input file validation report |")
+        echo("|--------------------------------------|")
 
         # overview
         self.landuse_summary()
@@ -33,16 +34,26 @@ class Report:
     def landuse_summary(self):
         """Print single line summary"""
         if self.landuse is None:
-            click.echo("Landuse classes: not checked.")
+            echo("Landuse classes: not checked.")
         else:
             msg = [
-                click.style('Landuse classes: invalid', fg='red') if self.landuse.n_errors + self.landuse.n_warnings > 0 else click.style('Landuse classes: valid', fg='green'),
+                style('Landuse classes: invalid', fg='red') if self.landuse.n_errors + self.landuse.n_warnings > 0 else style('Landuse classes: valid', fg='green'),
                 '\t\t', 
-                click.style(f'errors: {self.landuse.n_errors}', fg='red') if self.landuse.n_errors > 0 else 'errors: 0',
+                style(f'errors: {self.landuse.n_errors}', fg='red') if self.landuse.n_errors > 0 else 'errors: 0',
                 '\t ',
-                click.style(f'warnings: {self.landuse.n_warnings}', fg='yellow') if self.landuse.n_warnings > 0 else 'warnings: 0'
+                style(f'warnings: {self.landuse.n_warnings}', fg='yellow') if self.landuse.n_warnings > 0 else 'warnings: 0'
             ]
-            click.echo(''.join(msg))
+            echo(''.join(msg))
+
+            # append information about all landuse parameter files
+            for cl, lp in self.landuse.parameters.items():
+                valid = lp.n_errors + lp.n_warnings == 0
+                msg = f"CLASS {self.landuse.data[cl][1][:30]}:\t{'valid' if valid else 'invalid'}"
+                echo(
+                    style(msg, fg='green' if valid else 'red') + '\t\t' +
+                    style(f"errors: {lp.n_errors}", fg='red' if lp.n_errors > 0 else '') + '\t ' +
+                    style(f"warnings: {lp.n_warnings}", fg='yellow' if lp.n_warnings > 0 else '')
+                )
     
     def landuse_details(self, extended: bool = True):
         # check if there are invalid landuse classes
@@ -50,11 +61,11 @@ class Report:
         
         # print extended header
         if extended:
-            click.echo("Landuse-class definitions")
-            click.echo("-------------------------")
-            click.echo(f"PATH: {self.landuse.path}")
-            click.echo(f"NAME: {self.landuse.basename}")
-            click.echo(f"Total classes:    {len(self.landuse.data)}")
+            echo("Landuse-class definitions")
+            echo("-------------------------")
+            echo(f"PATH: {self.landuse.path}")
+            echo(f"NAME: {self.landuse.basename}")
+            echo(f"Total classes:    {len(self.landuse.data)}")
             click.secho(f"Invalid classes:  {n_inval}", fg='red' if n_inval > 0 else '')
 
         # without errors, don't give details about errors
@@ -63,23 +74,34 @@ class Report:
         
         # there are errors, so print them
         for cl, warn in self.landuse.errors.items():
+            # get the params if any
+            par = self.landuse.parameters.get(cl)
+            
+            # start printing
             if extended:
-                click.echo('')
-            n_err = len([1 for _ in warn if _[0].lower() != 'warning'])
-            n_wrn = len([1 for _ in warn if _[0].lower() == 'warning'])
+                echo('')
+            n_err = len([1 for _ in warn if _[0].lower() != 'warning']) + 0 if par is None else par.n_errors 
+            n_wrn = len([1 for _ in warn if _[0].lower() == 'warning']) + 0 if par is None else par.n_warnings
             name = self.landuse.data[cl][1]
+            ID = self.landuse.data[cl][0]
             if n_err + n_wrn == 0:
-                click.secho(f"{'+ ' if extended else ''}ClASS {name}: valid", fg='green')
+                click.secho(f"{'+ ' if extended else ''}ClASS {ID} {name}: valid", fg='green')
             else:
-                click.echo(click.style(f"{'+ ' if extended else ''}CLASS {name[:20]}: invalid\t\t\t", fg='red') + 
-                    click.style(f"errors: {n_err}", fg='red' if n_err > 0 else '') + '\t ' +
-                    click.style(f"warnings: {n_wrn}", fg='yellow' if n_wrn > 0 else ''))
+                echo(style(f"{'+ ' if extended else ''}CLASS {ID} {name[:20]}: invalid\t\t\t", fg='red') + 
+                    style(f"errors: {n_err}", fg='red' if n_err > 0 else '') + '\t ' +
+                    style(f"warnings: {n_wrn}", fg='yellow' if n_wrn > 0 else ''))
             
             # add the actual errors
             if extended:
                 for ty, msg in warn:
                     # TODO: the if is there, to supress color. Not yet sure if this is too colorful
                     click.secho(f"  - {msg}", fg= '' if False else ERROR_COLORS.get(ty.lower(), 'blue'))
+                
+                # add the warnings of the landuse file
+                if par is not None:
+                    click.echo(f'  PARAMETER FILE ({par.basename}):')
+                    for ty, msg in par.flat_errors:
+                        click.secho(f"  - {msg}", fg= '' if False else ERROR_COLORS.get(ty.lower(), 'blue'))
         
     def run(self):
         if self.output_file:
